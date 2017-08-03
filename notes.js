e72500b1463306a1630fb33ad4eadbc96882a1d4
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-03 23:42:54 +0700

    note

--- notes.js
+++ notes.js
@@ -1,6 +1,21 @@
 console.log('Starting notes.js');
-module.exports.age = 25;
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New notes';
+
+var addNote = (title,body) => {
+    console.log('Adding note',title,body)
+}
+var getAll = () => {
+    console.log('Getting all notes')
+}
+var getNote = (title) => {
+    console.log('Getting note',title)
+}
+var removeNote = (title) => {
+    console.log('Remove note',title)
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
