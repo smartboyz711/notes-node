d82d2130b9f89e575b37187868f175b15a5ed169
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-03 23:49:20 +0700

    note

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Starting app.js')
 
 const fs  = require('fs')
 const _ = require('lodash')
-const yargs = require('yargs');
+const yargs = require('yargs')
 
 const notes = require('./notes')
 
