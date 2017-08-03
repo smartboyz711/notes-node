3165aba9e212cfda8e538fc23bdace91584eea0c
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-03 22:22:31 +0700

    prepare

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 console.log('Starting app.js')
 
 const fs  = require('fs')
-const os  = require('os')
 const _ = require('lodash')
 const notes = require('./notes')
-
-// console.log(_.isString(true));
-// console.log(_.isString('tonz'));
-
-var filteredArray = _.uniq(['tonz',1,'tonz',1,2,3,4])
-console.log(filteredArray)
-
-
-//var res = notes.addNote();
-// var res = notes.add(9,-2);
-// console.log('result '+res);
-
-//var user = os.userInfo();
-
-//fs.appendFileSync('greeting.txt','hello '+user.username+' You are '+notes.age);
 
 
 
--- notes.js
+++ notes.js
@@ -4,6 +4,3 @@ module.exports.addNote = () => {
     console.log('addNote');
     return 'New notes';
 }
-module.exports.add = (a,b) => {
-   return a+b;
-}
