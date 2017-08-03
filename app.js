ee14f8fdd541f2b848cb3b3fac8b5d0b52592613
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-03 23:42:10 +0700

    note

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ console.log('Starting app.js')
 
 const fs  = require('fs')
 const _ = require('lodash')
+const yargs = require('yargs');
+
 const notes = require('./notes')
 
+const argv = yargs.argv
+var command = process.argv[2]
+console.log('command:',command)
+console.log('Yargs',argv)
+
+if(command == 'add'){
+    notes.addNote(argv.title,argv.body)
+}else if(command == 'list'){
+    notes.getAll()
+}else if(command == 'read'){
+    notes.getNote(argv.title)
+}else if(command == 'remove'){
+    notes.removeNote(argv.title)
+}
+else {
+    console.log('Command not recognized')
+}
 
 
