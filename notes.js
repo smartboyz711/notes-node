0decc83c5f510b8986b1df9dafa1b27ebfec9c07
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-05 13:04:12 +0700

    add note

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting notes.js');
 
+const fs = require('fs')
+
 var addNote = (title,body) => {
-    console.log('Adding note',title,body)
+    var notename = 'notes-data.json'
+    var notes = []
+    var note = {
+        title,
+        body
+    }
+
+    try{
+        var noteString = fs.readFileSync(notename)
+        notes = JSON.parse(noteString)
+    }catch (e) {
+        //console.log('File '+notename+' Not Found')
+    }
+
+    var duplicatNotes = notes.filter((note) => note.title === title)
+
+    if(duplicatNotes.length === 0){
+        notes.push(note)
+        fs.writeFileSync(notename,JSON.stringify(notes))
+    }
 }
 var getAll = () => {
     console.log('Getting all notes')
